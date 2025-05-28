7db51f7441a3004ff594c1d84147e6a2aa398de0
KBastidas07 <bastidaskevyn@gmail.com>
2025-05-28 17:18:08 -0500

    Actualización general del proyecto: ubicacion y Noticias

---
+++ src/pages/Kevyn1/Eventos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Eventos = () => {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>Eventos</h1>
+      <p>Aquí se mostrarán los próximos eventos y actividades.</p>
+    </div>
+  );
+};
+
+export default Eventos;
--- src/pages/Kevyn2/Ubicacion.js
+++ src/pages/Kevyn2/Ubicacion.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React from "react";
+import { FaMapMarkerAlt, FaClock } from "react-icons/fa";
+import miImagen from './Ubicacion.PNG';
+import "./Ubicacion.css"; // Importa el CSS
 
 const Ubicacion = () => {
   return (
-    <div>
-      <h1>Aqui va el contenido de ubicacion</h1>
-      <p>¡Descubre los mejores platos cerca de ti!</p>
+    <div className="ubicacion-container">
+      
+
+      <div className="contenedor-principal">
+        {/* Izquierda */}
+        <div className="lado izquierdo">
+          <div className="titulo-lado">
+            <FaMapMarkerAlt className="icono-rojo" />
+            <h3>Ubicación</h3>
+          </div>
+
+          <img
+            src={miImagen}
+            alt="Ubicación"
+            className="imagen-ubicacion"
+          />
+
+          <p className="direccion1">Cra. 23 #6a-38, Popayán, Cauca</p>
+           <p className="direccion2">A una cuadra del Parque Jose Maria Obando</p>
+        </div>
+
+        {/* Línea roja vertical */}
+        <div className="linea-roja" />
+
+        {/* Derecha */}
+        <div className="lado derecho">
+          <div className="titulo-lado">
+            <FaClock className="icono-rojo" />
+            <h3>Horarios de Atención</h3>
+          </div>
+
+          <ul className="lista-horarios">
+            <li>Lunes: Sin servicio</li>
+            <li>Martes - Viernes de 4:00 pm a 11:59 pm</li>
+            <li>Sabado - Domingo de 4:00 pm a 2:00 am</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
